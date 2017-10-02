19fd160544f18c60d90117dd795084b1377c1493
Gus Nordhielm <gnordhielm@gmail.com>
2017-10-02 06:54:05 -0700

    fix style import issue

--- src/root.module.js
+++ src/root.module.js
@@ -1,11 +1,12 @@
 
 import angular from 'angular'
 import '@uirouter/angularjs'
+import 'angular-animate'
 
 import './root.style.scss'
 import router from './root.router.js'
 
 angular
-	.module('root', ['ui.router'])
+	.module('root', ['ui.router', 'ngAnimate'])
 	.config(router)
 
