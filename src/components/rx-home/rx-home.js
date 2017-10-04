597b7727a8b63858846767699910df433b27e468
Gus Nordhielm <gnordhielm@gmail.com>
2017-10-03 20:49:00 -0700

    update server

---
+++ src/components/rx-home/rx-home.js
@@ -0,0 +1,13 @@
+
+import template from './rx-home.html'
+
+angular
+	.module('root')
+	.component('rxHome', {
+		template, controller,
+		bindings: {}
+	})
+
+function controller() {
+	
+}
---
+++ src/root.js
@@ -0,0 +1,15 @@
+
+// Libraries
+
+import angular from 'angular'
+
+// Configuration
+
+import './root.module.js'
+
+// Stylesheets
+
+import './root.styles.scss'
+
+// Components
+import './components/rx-home/rx-home.js'
